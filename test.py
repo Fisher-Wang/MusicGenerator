56dba8d761d12761b07da6e9b4ec77b12b1ee707
Fisher-Wang <kingwfsh@163.com>
2021-11-26 23:53:25 +0800

    add dataset (bach clementi mozart)

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
+from utils import setup_lib
+setup_lib()
+
 import os
-
-workingdir = os.path.abspath('.')
-dllspath = os.path.join(workingdir, 'fluidsynth-2.2.4-win10-x64', 'bin')
-os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
-
-from ctypes.util import find_library
-lib = find_library("libfluidsynth")
-if lib is None:
-    print("libfluidsynth not found!")
-
 from mingus.containers import Note, NoteContainer, Bar
 from mingus.midi import fluidsynth
 
-soundfont_path = os.path.join(workingdir, "FluidR3_GM", "FluidR3_GM.sf2")
+soundfont_path = os.path.join("FluidR3_GM", "FluidR3_GM.sf2")
 fluidsynth.init(soundfont_path)
 
 b = Bar(key='C', meter=(10,8))
