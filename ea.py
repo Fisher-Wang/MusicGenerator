9b2f4d0ac70d53b10cea862248f74c63dd4e3940
CYQ <1900012725@pku.edu.cn>
2021-12-11 14:14:57 +0800

    reverse, invert, move added to EA

--- ea.py
+++ ea.py
@@ -50,13 +50,7 @@ net.load_state_dict(torch.load("net_final.pkl"))
 
 class EvolutionController:
     def __init__(self, mutate_prob=0.1, population_size=100, n_evolution=100, parent_fraction=0.5, mutation_fraction=0.5, crossover_fraction=0, log_path='output/'):
-        # evolution hyper-parameters
-        # self.n_blocks_mutate_prob = kwargs.get('n_blocks_mutate_prob', 0.1)
-        # self.n_base_channels_mutate_prob = kwargs.get('n_base_channels_mutate_prob', 0.5)
         self.mutate_prob = mutate_prob
-        # self.n_blocks_mutate_prob = kwargs.get('n_blocks_mutate_prob', 0.1)
-        # self.n_channels_mutate_prob = kwargs.get(
-        #     'n_base_channels_mutate_prob', 0.1)
         self.population_size = population_size
         self.n_generations = n_evolution
         self.parent_num = int(self.population_size*parent_fraction)
@@ -98,7 +92,6 @@ class EvolutionController:
     def mutation(self,parents):
         for _ in range(self.mutation_num):
             selected_parent = parents[np.random.randint(self.parent_num)]
-            # Mutate
             child = selected_parent.mutate()
             self.add_individual(child)
     
@@ -113,11 +106,8 @@ class EvolutionController:
 
         best_score_history = []
         history_change=''
-        #print('Start Evolution Search...')
         t=tqdm(range(self.n_generations),desc="Evolutionary Search")
         for generation in t: 
-            #print(f'Start Generation={generation}')
-            # sort
             sorted_inds=np.argsort(self.scores)[::-1]
             parents = [self.population[_] for _ in sorted_inds[:self.parent_num]]
 
@@ -127,7 +117,6 @@ class EvolutionController:
                 history_change+='+'
             else:
                 history_change+='-'
-            # print(f'EA Generation={generation} Now Best score: {now_best_score} Best Individual {parents[0]}')
             self.log_file.write(
                 f"==={generation}/{self.n_generations}===\n")
             for i in sorted_inds[:3]:
@@ -149,30 +138,6 @@ class EvolutionController:
         #print('Finish Evolution Search')
         ind=np.argmax(self.scores)
         return self.population[ind],self.scores[ind]
-
-import multiprocessing as mp
-
-
-def individual_gen_process(pid,individual_generator):
-    # print(f"start {pid}")
-    sys.stdout = open("output/individual.out", "a+")
-    individual=individual_generator()
-    score=individual.evaluate()
-    return individual,score
-
-def individual_mutation_process(pid,parent):
-    # print(f"start {pid}")
-    sys.stdout = open("output/individual.out", "a+")
-    child=parent.mutate()
-    score=child.evaluate()
-    return child,score
-
-def individual_crossover_process(pid,parents):
-    # print(f"start {pid}")
-    sys.stdout = open("output/individual.out", "a+")
-    child=parents[0].crossover(*parents)
-    score=child.evaluate()
-    return child,score
 
 
 class MusicIndividual:
@@ -198,8 +163,23 @@ class MusicIndividual:
     
     def mutate(self):
         child_music=copy.deepcopy(self.music)
-        index = random.randint(0, self.length - 1)
-        child_music[index] = random.randint(0, self.note_num)
+        p = random.random()
+        if p < 0.7: #变异就是随机替换一个音符为随机值
+            index = random.randint(0, self.length - 1)
+            child_music[index] = random.randint(0, self.note_num)
+        elif p < 0.8: #随机移调
+            move = random.randint(-8, 8)
+            for i in range(self.length):
+                child_music[i] = child_music[i] + move
+                if child_music[i] < 0:
+                    child_music[i] = child_music[i] + self.note_num
+                if child_music[i] > self.note_num:
+                    child_music[i] = child_music[i] - self.note_num
+        elif p < 0.9: #倒影变换
+            for i in range(self.length):
+                child_music[i] = self.note_num - child_music[i]
+        else:         #逆行变换
+            child_music = child_music.reverse()
         child = MusicIndividual(music=child_music, length=self.length)
         return child
     
@@ -222,7 +202,7 @@ class MusicIndividual:
 
 
 
-controller=EvolutionController(population_size=1000, crossover_fraction=0.2)
+controller=EvolutionController(population_size=10, crossover_fraction=0.2)
 print(controller.population_size)
                 
 for individual_i in range(controller.population_size):
